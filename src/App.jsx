e23bee9f9a679519bd787a04ed5c37234a6a9a07
diegonacif <diegoreisnacif@gmail.com>
2023-04-16 14:12:55 -0300

    refactor: header in app component

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,33 @@ export const App = () => {
 
   return (
     <div className="App 
-    flex flex-col justify-center items-center h-full min-h-screen
+    flex flex-col items-center h-screen
     bg-neutral-800 text-gray-100
-    subpixel-antialiased"> 
+    subpixel-antialiased box-border"> 
       {
         room ? 
-        <Chat room={room} /> : 
+        <>
+          <div className="header 
+            flex justify-between w-full px-4 py-2
+            bg-gradient-to-bl from-indigo-600"
+          >
+            <h1 className="text-2xl font-bold">Sala: {room.toUpperCase()}</h1>
+            <div className="sign-out">
+              <button 
+                className="bg-gradient-to-br from-indigo-600 to-indigo-900
+                font-medium
+                px-4 py-2 rounded border-2 border-style-solid border-neutral-800
+                active:hover:border-indigo-500
+                md:hover:border-indigo-500
+                transition-colors select-none"
+                onClick={signUserOut}
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+          <Chat room={room} />
+        </> :
         <div className="room flex flex-col md:flex-row items-center gap-4">
           <label className="text-indigo-400 font-bold">Enter Room Name:</label>
           <input ref={roomInputRef} className="w-48 h-8 px-2 text-gray-900 rounded" />
@@ -53,19 +74,7 @@ export const App = () => {
         </div>
       } 
 
-      <div className="sign-out mt-10 md:mt-8">
-        <button 
-          className="bg-gradient-to-br from-indigo-600 to-indigo-900
-          font-medium
-          px-4 py-2 rounded border-2 border-style-solid border-neutral-800
-          active:hover:border-indigo-500
-          md:hover:border-indigo-500
-          transition-colors select-none"
-          onClick={signUserOut}
-        >
-          Sign Out
-        </button>
-      </div>
+      
     </div>
   )
   
--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -41,36 +41,39 @@ export const Chat = (props) => {
     setNewMessage("");
   }
 
-  console.log(messages);
-
   return (
-    <div className="chat-container w-screen px-4">
-      <div className="header">
+    <div className="chat-container 
+      grid grid-rows-[auto_min-content]
+      h-full w-screen px-4 overflow-scroll"
+    >
+      {/* <div className="header bg-gradient-to-b from-indigo-600">
         <h1 className="text-2xl font-bold">Sala: {room.toUpperCase()}</h1>
-      </div>
+      </div> */}
 
-      <div className="messages flex flex-col w-full gap-y-3 mt-4">
+      <div className="messages flex flex-col w-full max-w-full max-h-full overflow-y-scroll gap-y-3 py-4">
         {
           messages.map((message) => (
             <div 
-              className={`message flex w-max max-w-full px-2 py-1 rounded
-              bg-neutral-700 
+              className={`message flex flex-col w-fit max-w-full px-3 py-2 rounded
               md:max-w-lg
-              ${message.user == auth.currentUser.displayName && 'self-end'}`}
+              ${message.user == auth.currentUser.displayName ?
+              'bg-indigo-900' : 'bg-neutral-700'}
+              ${message.user == auth.currentUser.displayName &&
+              'self-end'}`}
               key={message.id}
             >
               <span className="user min-w-max font-bold">
                 {message.user}:
               </span>
-              <p className="ml-2">{message.text}</p>
+              <p className="mt-1">{message.text}</p>
             </div>
           ))
         }
       </div>
 
-      <form onSubmit={handleSubmit} className="new-message-form flex gap-x-4 mt-4">
+      <form onSubmit={handleSubmit} className="new-message-form flex gap-x-4 pb-4">
         <input 
-          className="new-message-input px-2 text-gray-900 rounded" 
+          className="new-message-input w-full px-2 text-gray-900 rounded" 
           placeholder="Type your message here..." 
           onChange={(e) => setNewMessage(e.target.value)}
           value={newMessage}
